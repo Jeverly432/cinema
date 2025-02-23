62685982cae497046b392e9f4415c3c0007d5c93
Jeverly432 <nomoconov.sasha@yandex.ru>
2025-02-23 14:03:25 +0900

    fix storybook

--- config/storybook/preview.tsx
+++ config/storybook/preview.tsx
@@ -1,6 +1,6 @@
 import type { Preview } from "@storybook/react";
 import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator";
-import "app/styles/_index.scss";
+import "../../src/app/styles/_index.scss";
 import { RouterDecorator } from "../../src/shared/config/storybook/RouterDecorator";
 
 const preview: Preview = {
