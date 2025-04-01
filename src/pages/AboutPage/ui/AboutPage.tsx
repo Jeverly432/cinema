ffe0af4f02daca0b90aa81b5f74ea4e2ce13dd6e
Alex <94704647+Jeverly432@users.noreply.github.com>
2025-04-02 00:28:31 +0900

    Revert "fix errors"

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -7,6 +7,7 @@ const AboutPage = () => {
     return (
         <>
             About Page
+            <Counter />
         </>
     );
 };
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -6,6 +6,7 @@ const MainPage = () => (
     <>
         <Banner />
         <Categories />
+        <Counter />
     </>
 );
 
